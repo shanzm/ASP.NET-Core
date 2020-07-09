2bac020ae9f688c29dac3875ef0e57de31203cd4
shanzm_aimu <2351144690@qq.com>
2020-07-09 18:10:37 +0800

    2020年7月9日 18:10:30- Update

--- 004Environment/Program.cs
+++ 004Environment/Program.cs
@@ -23,7 +23,7 @@ namespace _004Environment
                 {
                     //webBuilder.UseStartup<Startup>();
                     //注意若是需要使用我们自定义的Startup类配置，这里需要将上句代码修改为下面：
-                    //webBuilder.UseStartup(Assembly.GetExecutingAssembly().FullName);
+                    webBuilder.UseStartup(Assembly.GetExecutingAssembly().FullName);
                     //获取当前的程序集的名称，在当前程序集中找含有命名中含有Demo的Startup类（因为我们在launchSettings设置的环境名为Demo）
                     //若是没有Startup类则再使用默认的Startup类
                 });
